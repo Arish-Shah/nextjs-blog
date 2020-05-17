d053c03ee61f12c50531537917566a3be10d4cc9
Arish-Shah <arish0246@gmail.com>
2020-05-17 18:22:27 +0530

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,49 @@
+import Head from 'next/head';
 import Link from 'next/link';
-import fs from 'fs';
 
-const Home = ({ slugs }) => {
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Date from '../components/date';
+import { getSortedPostsData } from '../lib/posts';
+
+export default function Home({ allPostsData }) {
   return (
-    <div>
-      {slugs.map((slug) => (
-        <div key={slug}>
-          <Link href={'blog/' + slug}>
-            <a>{'blog/' + slug}</a>
-          </Link>
-        </div>
-      ))}
-    </div>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I like Web Development</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   );
-};
+}
 
-export const getStaticProps = async () => {
-  const files = fs.readdirSync('posts');
-  const slugs = files.map((file) => file.replace('.md', ''));
-
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      slugs
+      allPostsData
     }
   };
-};
-
-export default Home;
+}
